ed0844789e06ac6c6177f5276f7fba1ef783662a
Ch34k0 <reiko-schulz@hotmail.de>
2018-04-09 16:05:59 +0200

    Funktion geschrieben

--- test_biblio/Test_Klasse/Test_Klasse.cs
+++ test_biblio/Test_Klasse/Test_Klasse.cs
@@ -8,5 +8,20 @@ namespace Test_Klasse
 {
     public class Test_Klasse
     {
+        public static List<int> Start_Zahlen_testen(int unter, int ober)
+        {
+            var zahlen = Zahlen_testen(unter, ober);
+            return zahlen;
+        }
+
+        internal static List<int> Zahlen_testen(int unter, int ober)
+        {
+            var zahlen = new List<int>();
+            for (int zahl = unter; zahl <= ober; zahl++)
+            {
+                zahlen.Add(zahl);
+            }
+            return zahlen;
+        }
     }
 }
