868d6d8cd8a4e585693b9eb04be365e3df29a948
Raiko Schulz <SchulzRa@BAIT.BBW.DD>
2018-04-10 08:28:25 +0200

    Testen

--- test_biblio/Test_Klasse/Test_Klasse.cs
+++ test_biblio/Test_Klasse/Test_Klasse.cs
@@ -3,25 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using test_biblio;
+using NUnit.Framework;
 
 namespace Test_Klasse
 {
+    [TestFixture]
     public class Test_Klasse
     {
-        public static List<int> Start_Zahlen_testen(int unter, int ober)
+        [Test, Category("Akzeptanztest")]
+        public void Zahlen_zwischen_unten_und_oben()
         {
-            var zahlen = Zahlen_testen(unter, ober);
-            return zahlen;
+            var ergebnis = Test_Biblio.Start_Zahlen_testen(3, 5);
+            Assert.AreEqual(new[] { 3, 4, 5 }, ergebnis.ToArray());
         }
 
-        internal static List<int> Zahlen_testen(int unter, int ober)
+        [Test, Category("Gerüsttest")]
+        public void Anzahl_Zahlen()
         {
-            var zahlen = new List<int>();
-            for (int zahl = unter; zahl <= ober; zahl++)
-            {
-                zahlen.Add(zahl);
-            }
-            return zahlen;
+            var ergebnis = Test_Biblio.Zahlen_testen(-1, 45);
+            Assert.AreEqual(46,ergebnis.Count());
         }
     }
 }
--- test_biblio/test_biblio/Test_Biblio.cs
+++ test_biblio/test_biblio/Test_Biblio.cs
@@ -8,5 +8,21 @@ namespace test_biblio
 {
     public class Test_Biblio
     {
+        public static List<int> Start_Zahlen_testen(int unter, int ober)
+        {
+            var zahlen = Zahlen_testen(unter, ober);
+            return zahlen;
+
+        }
+
+        internal static List<int> Zahlen_testen(int unter, int ober)
+        {
+            var zahlen = new List<int>();
+            for (int zahl = unter; zahl <= ober; zahl++)
+            {
+                zahlen.Add(zahl);
+            }
+            return zahlen;
+        }
     }
 }
