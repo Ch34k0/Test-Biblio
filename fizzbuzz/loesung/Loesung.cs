0e5864b286c721fc1b8b0ebb27af91ca9b8d4d6e
Ch34k0 <reiko-schulz@hotmail.de>
2018-04-11 10:56:10 +0200

    Version mit anderen Namen

--- fizzbuzz/loesung/Loesung.cs
+++ fizzbuzz/loesung/Loesung.cs
@@ -13,9 +13,9 @@ namespace loesung
             int unter = 0, ober = 0;            
             var unten = Eingabe_Unten(unter);
             var oben = Eingabe_Ober(ober);
-            var zahlen = EingabeZahlen(unten, oben);
-            var zahlenfb = Verarbeitung(zahlen, unten, oben);
-            //var ergebnis = Ausgabe(zahlenfb, unten);
+            var zahlen_list = EingabeZahlen(unten, oben);
+            var zahlen_dic = Verarbeitung(zahlen_list, unten, oben);
+            //var ergebnis = Ausgabe(zahlen_dic, unten);
         }
 
         public static int Eingabe_Unten(int unter)
@@ -40,45 +40,45 @@ namespace loesung
 
         public static List<int> EingabeZahlen(int unter, int ober)
         {
-            var zahlen = new List<int>();
+            var zahlen_list = new List<int>();
             for (int zahl = unter; zahl < ober; zahl++)
             {
-                zahlen.Add(zahl);
+                zahlen_list.Add(zahl);
             }
-            return zahlen;
+            return zahlen_list;
         }
 
-        public static Dictionary<int, string> Verarbeitung(List<int> zahlen, int unten, int oben)
+        public static Dictionary<int, string> Verarbeitung(List<int> zahlen_list, int unten, int oben)
         {
-            var zahlenfb = new Dictionary<int, string>();
+            var zahlen_dic = new Dictionary<int, string>();
             for (int zahl = unten; zahl <= oben + unten; zahl++)
             {                
                 if (zahl % 3 == 0 && zahl % 5 == 0)
                 {
-                    zahlenfb.Add(zahl, "fizzbuzz");
+                    zahlen_dic.Add(zahl, "fizzbuzz");
                 }               
                 else if (zahl % 3 == 0)
                 {
-                    zahlenfb.Add(zahl, "fizz");
+                    zahlen_dic.Add(zahl, "fizz");
                 }                
                 else if (zahl % 5 == 0)
                 {
-                    zahlenfb.Add(zahl, "buzz");
+                    zahlen_dic.Add(zahl, "buzz");
                 }
                 else
                 {
-                    zahlenfb.Add(zahl, zahl.ToString());
+                    zahlen_dic.Add(zahl, zahl.ToString());
                 }
             }
-            return zahlenfb;
+            return zahlen_dic;
         }
 
-        //public static Dictionary<int, string> Ausgabe(Dictionary<int, string> zahlenfb, int unter)
+        //public static Dictionary<int, string> Ausgabe(Dictionary<int, string> zahlen_dic, int unter)
         //{
         //    var ergebnis = new Dictionary<int, string>();
-        //    for (int zahl = unter; zahl < zahlenfb.Count; zahl++)
+        //    for (int zahl = unter; zahl < zahlen_dic.Count; zahl++)
         //    {
-        //        Console.WriteLine(zahlenfb[zahl]);
+        //        Console.WriteLine(zahlen_dic[zahl]);
         //    }
         //    Console.Read();
         //    return ergebnis;
