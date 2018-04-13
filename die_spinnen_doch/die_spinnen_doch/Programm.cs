13383ca73ab45991b3e135383c5024d209e0be13
Raiko Schulz <SchulzRa@BAIT.BBW.DD>
2018-04-13 11:49:40 +0200

    Neu

--- die_spinnen_doch/die_spinnen_doch/Programm.cs
+++ die_spinnen_doch/die_spinnen_doch/Programm.cs
@@ -11,68 +11,36 @@ namespace die_spinnen_doch
     {
         public static string Main(string dezimal_zahl)
         {
-            var ergebnis = Uebersetzen(dezimal_zahl);
-            return ergebnis;
+            var ergebnis = Zahl_Teilen(dezimal_zahl);
+            Position_bestimmen(ergebnis);
+            string ende = Zusammenfuehren(ergebnis, String.Empty);
+            return ende;
         }
 
-        public static string Uebersetzen(string dezimal_zahl)
+        public static string[] Zahl_Teilen(string dezimal_zahl)
         {
-            int position=0;
-            bool richtung=false;
-            string temporaer=String.Empty;
+            var ergebnis = dezimal_zahl.Split();
             
-            Dictionary<int, string> romantext = new Dictionary<int, string>()
-            { {  0, "M" }, { 1, "D" }, { 2, "C" }, { 3, "L" }, { 4, "X" },{ 5, "V" },{ 6, "I" } };
-            Dictionary<int, int> romanzahl = new Dictionary<int, int>()
-            { {  0, 1000 },{ 1, 500 },{ 2, 100 }, { 3,50},{ 4,10 }, { 5,5 }, { 6,1 } };
-
-            var ergebnis = Ueberschreiben(dezimal_zahl,  romantext,  romanzahl, position, richtung, temporaer);
             return ergebnis;
         }
-        public static string Ueberschreiben(string dezimal_zahl,Dictionary<int,string> romantext,Dictionary<int,int>romanzahl,int position,bool richtung, string temporaer)
+
+        public static void Position_bestimmen(string[] dezimal_zahl)
         {
-            string ergebnis="";
-            for (int zeichen = dezimal_zahl.Length; zeichen > 0; zeichen--)
+            var ergebnis = 0;
+            
+        }
+
+        public static string Zusammenfuehren(string[] dezimal_zahl, string ergebnis)
+        {
+            ergebnis = Zusammen_Schleife(dezimal_zahl, ergebnis);
+            return ergebnis;
+        }
+
+        public static string Zusammen_Schleife(string[] dezimal_zahl, string ergebnis)
+        {
+            for (int i = 0; i < dezimal_zahl.Length; i++)
             {
-                position = Convert.ToInt32(dezimal_zahl.Substring(zeichen - 1, 1)) * Convert.ToInt32(Math.Pow(10, dezimal_zahl.Length - zeichen));
-                while (position > 0)
-                {
-                    richtung = false;
-
-                    for (int k = 6; k > 0; k--)
-                    {
-
-                        if (romanzahl[k] - position > 0)
-                        {
-                            if ((romanzahl[k] - position) == Convert.ToInt32(Math.Pow(10, dezimal_zahl.Length - zeichen)))
-                            {
-                                position = romanzahl[k] - position;
-                                temporaer = romantext[k] + temporaer;
-                                richtung = true;
-                                break;
-                            }
-                        }
-                    }
-
-                    for (int i = 0; i < 7; i++)
-                    {
-                        if (position - romanzahl[i] >= 0)
-                        {
-                            position = position - romanzahl[i];
-                            if (richtung == false)
-                            {
-                                temporaer = temporaer + romantext[i];
-                            }
-                            else
-                            {
-                                temporaer = romantext[i] + temporaer;
-                            }
-                            break;
-                        }
-                    }
-                }
-                ergebnis = temporaer + ergebnis;
-                temporaer = "";
+                ergebnis += dezimal_zahl[i];
             }
             return ergebnis;
         }
